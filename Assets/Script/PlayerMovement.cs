23742dbbe51cc89dc3cb623fca855f04eda62828
generasirabbani <generasirabbani@apps.ipb.ac.id>
2024-03-26 00:00:21 +0700

    added temporary dash at playermovement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,15 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     Animator animator;
     Vector2 moveInput;
+    Vector3 moveDir;
+    float startTime;
     [SerializeField] CharacterController characterController; 
     // [SerializeField] Transform groundCheck;
     // [SerializeField] LayerMask ground;
     [SerializeField] AudioSource jumpSound;
     [SerializeField] MovementComponent movementComponent;
     [SerializeField] PlayerAim aimScript;
-    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float moveSpeed = 10f;
     [SerializeField] float maxMoveSpeed = 80f; 
     [SerializeField] float minMoveSpeed = 5f; 
+    [SerializeField] float dashSpeed = 30f; 
+    [SerializeField] float dashTime = 0.2f; 
+    [SerializeField] float dashCooldown = 2f; 
+    [SerializeField] KeyCode dashInput = KeyCode.Space; 
 
     // Start is called before the first frame update
     void Start()
@@ -32,17 +38,33 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        Vector3 MoveDir = movementComponent.InputToWorldDir(moveInput);
+        moveDir = movementComponent.InputToWorldDir(moveInput);
 
-        characterController.Move(MoveDir * Time.deltaTime * moveSpeed);
+        characterController.Move(moveDir * Time.deltaTime * moveSpeed);
 
-        float forward = Vector3.Dot(MoveDir, transform.forward);
-        float right = Vector3.Dot(MoveDir, transform.right);
+        float forward = Vector3.Dot(moveDir, transform.forward);
+        float right = Vector3.Dot(moveDir, transform.right);
 
         animator.SetFloat("forwardSpeed", forward);
         animator.SetFloat("rightSpeed", right);
 
         characterController.Move(Vector3.down * Time.deltaTime * 10f);
-        aimScript.UpdateAim(MoveDir);
+        aimScript.UpdateAim(moveDir);
+
+        if(Input.GetKeyDown(dashInput) && Time.time - startTime >= dashCooldown)
+        {
+            animator.SetTrigger("dash");
+            StartCoroutine(Dash());
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        startTime = Time.time;
+        while(Time.time < startTime + dashTime)
+        {
+            characterController.Move(moveDir * Time.deltaTime * dashSpeed);
+            yield return null;
+        }
     }
 }
