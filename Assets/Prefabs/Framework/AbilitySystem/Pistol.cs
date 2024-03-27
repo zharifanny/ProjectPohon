6ad20ad0b42fd031846cae72da1b6136e3271399
Khindy Zharifany <zharifanny@gmail.com>
2024-03-27 09:30:44 +0700

    added khindy branch

---
+++ Assets/Prefabs/Framework/AbilitySystem/Pistol.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    [SerializeField] AimComponent aimComp;
+    [SerializeField] float Damage = 5f;
+
+    public override void Attack()
+    {
+        Vector3 aimDir;
+        GameObject target = aimComp.GetAimTarget(out aimDir);
+        DamageGameObject(target, Damage);
+    }
+}
---
+++ Assets/Prefabs/Framework/AbilitySystem/SpeedBoostAbility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoostAbility : MonoBehaviour
+{
+    [SerializeField] float boostAmt = 20f;
+    [SerializeField] float boostDuration = 2f;
+    Player Player;
+
+    public override void ActivateAbility()
+    {
+        if (!CommitAbility()) return;
+        Player = AbilityComp.GetComponent<Player>();
+        Player.AddMoveSpeed(boostAmt);
+
+    }
+}
